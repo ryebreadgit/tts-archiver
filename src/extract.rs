551875e73ae177f625680e2c0e77d98c6da1b633
Rylan <rye@rye.moe>
2024-10-25 02:04:26 -0600

    Add regex for non http/s links but comment out as we need to find a better method for this

--- src/extract.rs
+++ src/extract.rs
@@ -8,12 +8,37 @@ async fn get_url_regex() -> Result<Regex, Box<dyn Error>> {
 pub async fn get_urls_from_str(s: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut urls: Vec<String> = Vec::new();
     let url_regex = get_url_regex().await?;
+    
+    /* Find a better method for this
+    let json_url_regex = get_json_url_regex().await?;
 
-    let s = s.replace("http://cloud-3.steamusercontent.com/", "https://steamusercontent-a.akamaihd.net/"); // fix for updated url format, must be updated in json manually
+    // Fix URLs in JSON that don't start with http/s://
+    let s = json_url_regex.replace_all(s, |caps: &regex::Captures| {
+        let matched = caps.get(1).unwrap().as_str();
+        if !matched.starts_with("http://") && !matched.starts_with("https://") {
+            format!(r#""URL":"https://{}""#, matched)
+        } else {
+            format!(r#""URL":"{}""#, matched)
+        }
+    });
+     */
 
+    // Replace specific URL patterns
+    let s = s.replace(
+        "http://cloud-3.steamusercontent.com/",
+        "https://steamusercontent-a.akamaihd.net/"
+    );
+
+    // Collect all URLs
     for cap in url_regex.captures_iter(&s) {
         if let Some(url) = cap.get(0) {
-            let url_str = url.as_str().to_string();
+            let mut url_str = url.as_str().to_string();
+            
+            // Add https:// to URLs that don't have a protocol
+            if !url_str.starts_with("http://") && !url_str.starts_with("https://") {
+                url_str = format!("https://{}", url_str);
+            }
+            
             urls.push(url_str);
         }
     }
